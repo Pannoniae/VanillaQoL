8b88bbb4eb4a546eda00b77633ba350051c8d81b
pannoniae <pannoniae@users.noreply.github.com>
2023-12-25 21:35:40 +0000

    .

--- VanillaQoL.cs
+++ VanillaQoL.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using Consolaria.Content.Items.Placeable;
 using MagicStorage.Common.Systems;
 using MonoMod.Cil;
 using Terraria.ModLoader;
